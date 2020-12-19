efb37437876d47ceeff8ba09e5d1d58f2ba128cc
amrMohamedFoad <amrfcis0101@gmail.com>
2020-12-19 12:18:48 +0200

    build desgin and orderForm controllers and pages

---
+++ src/main/java/tacos/OrderController.java
@@ -0,0 +1,38 @@
+package tacos;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+
+	@GetMapping("/current")
+	public String orderForm(Model model) {
+		
+		//create order object to pass to form
+		model.addAttribute("order", new Order());
+		return "orderForm";
+	}
+	
+	@PostMapping
+	public String processOrder(@Valid Order order, Errors errors) {
+		
+		if(errors.hasErrors()) {
+			return "orderForm"; //stay in the same page
+		}
+		
+		//else
+		log.info("Order submitted: " + order);
+        return "redirect:/"; // go to home
+	}
+}
