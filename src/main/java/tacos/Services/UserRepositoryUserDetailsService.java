04740b4b5483e391523f6cc0e8b996227ce69349
amrMohamedFoad <amrfcis0101@gmail.com>
2021-01-02 15:20:59 +0200

    configure spring security to use userDetails service

---
+++ src/main/java/tacos/Services/UserRepositoryUserDetailsService.java
@@ -0,0 +1,33 @@
+package tacos.Services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import tacos.Model.User;
+import tacos.Repos.UserRepository;
+
+@Service
+public class UserRepositoryUserDetailsService implements UserDetailsService {
+
+	private UserRepository userRepo;
+
+	@Autowired
+	public UserRepositoryUserDetailsService(UserRepository userRepo) {
+		this.userRepo = userRepo;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		User user = userRepo.findByUsername(username);
+		
+		if (user != null) 
+			return user;
+		
+		throw new UsernameNotFoundException("User '" + username + "' not found");
+	}
+
+}
--- src/main/java/tacos/configs/SecurityConfig.java
+++ src/main/java/tacos/configs/SecurityConfig.java
@@ -7,21 +7,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
+
+import tacos.Services.UserRepositoryUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	private DataSource dataSource;
+	private UserRepositoryUserDetailsService userDetailsService;
 
-	@SuppressWarnings("deprecation")
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.jdbcAuthentication().dataSource(dataSource)
-				.usersByUsernameQuery("select username, password, enabled from Users " + "where username=?")
-				.authoritiesByUsernameQuery("select username, authority from UserAuthorities " + "where username=?")
-				.passwordEncoder(new StandardPasswordEncoder("53cr3t"));
+		auth.userDetailsService(userDetailsService);
 	}
 }
+	
