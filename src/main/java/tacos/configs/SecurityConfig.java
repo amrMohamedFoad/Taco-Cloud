574f43e66507d3138b3dcdeb80861ef74a9210ed
amrMohamedFoad <amrfcis0101@gmail.com>
2021-01-02 13:16:44 +0200

    build security config class

---
+++ src/main/java/tacos/configs/SecurityConfig.java
@@ -0,0 +1,24 @@
+package tacos.configs;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery("select username, password, enabled from Users " + "where username=?")
+				.authoritiesByUsernameQuery("select username, authority from UserAuthorities " + "where username=?");
+	}
+}
